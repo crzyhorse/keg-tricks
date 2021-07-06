42fa2b656ae7982b17109575ac65924daf6da273
crzyhorse <ehambright@gmail.com>
2021-07-05 19:45:14 -0700

    Added simple monitor script for raspberry pi4

---
+++ pi-4/monitor.py
@@ -0,0 +1,11 @@
+import serial
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/serial0',115200, timeout=1)
+    ser.flush()
+    
+    while True:
+
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
